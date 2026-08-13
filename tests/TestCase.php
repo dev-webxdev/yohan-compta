43ec8c0fff8ba63070d1d0dacbeb59607f81010f
Dingedi <64697405+dingedi@users.noreply.github.com>
2026-08-13 18:58:58 +0200

    Make feature tests time-independent

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Carbon;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Carbon::setTestNow('2026-11-01 12:00:00');
+    }
+
+    protected function tearDown(): void
+    {
+        Carbon::setTestNow();
+        parent::tearDown();
+    }
 }
