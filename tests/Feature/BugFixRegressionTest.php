0a6137235678214e8231f924d7887deb0ef1c6d8
Dingedi <64697405+dingedi@users.noreply.github.com>
2026-08-14 17:41:03 +0200

    Fix #4 align weekly summary values

--- tests/Feature/BugFixRegressionTest.php
+++ tests/Feature/BugFixRegressionTest.php
@@ -66,6 +66,18 @@ final class BugFixRegressionTest extends TestCase
         self::assertStringNotContainsString('zoom:1.5', $css);
     }
 
+    public function test_week_totals_stay_next_to_labels_and_only_show_net_overtime_amount(): void
+    {
+        $this->get('/mois/2026-08')->assertOk()->assertSee('Montant sup :');
+        $css = file_get_contents(public_path('app.css'));
+        $view = file_get_contents(resource_path('views/month.blade.php'));
+
+        self::assertStringNotContainsString('.week-metrics b{float:right', $css);
+        self::assertStringContainsString('.week-metrics b{float:none;margin-left:4px}', $css);
+        self::assertStringContainsString("Money::formatCents(\$week['overtime_net_cents']) }} net</b>", $view);
+        self::assertStringNotContainsString("\$week['overtime_gross_cents']", $view);
+    }
+
     public function test_future_payment_is_not_shown_as_received_on_dashboard(): void
     {
         OvertimePayment::query()->create([
